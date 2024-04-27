a66d9131ec2c3b555daee596a9fcdc7ce62b71d8
Al Samir <shahinur1591@gmail.com>
2024-04-27 22:08:43 +0600

    Database connected

--- api/touristSport.js
+++ api/touristSport.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const client = require("../mongoDB/mongoDB");
 
 router.get("/", (req, res) => {
   res.send("Tourist Sport api");
   console.log("Tourist Sport api");
 });
+
+async function run() {
+  try {
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir);
 
 module.exports = router;
---
+++ mongoDB/mongoDB.js
@@ -0,0 +1,11 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.DATABASE_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+module.exports = client;

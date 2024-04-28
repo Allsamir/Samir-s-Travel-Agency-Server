6b79db7612155032ed477ee891f03efd8ef9b7d4
Al Samir <shahinur1591@gmail.com>
2024-04-28 15:25:43 +0600

    details api created

--- api/touristSport.js
+++ api/touristSport.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const client = require("../mongoDB/mongoDB");
 const database = client.db("travelAgencyDB");
 const touristSports = database.collection("touristSports");
+const { ObjectId } = require("mongodb");
 
 async function run() {
   try {
@@ -11,8 +12,15 @@ async function run() {
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
     );
-    router.get("/", (req, res) => {
-      res.send("Tourist Sport api");
+    router.get("/", async (req, res) => {
+      const cursor = touristSports.find({});
+      const allTouristSports = await cursor.toArray();
+      res.json(allTouristSports);
+    });
+    router.get("/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await touristSports.findOne({ _id: new ObjectId(id) });
+      res.json(result);
     });
     router.post("/", async (req, res) => {
       const touristSport = req.body;
